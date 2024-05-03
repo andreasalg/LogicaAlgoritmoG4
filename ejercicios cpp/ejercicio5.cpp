ad0113238becdae531071fbe27c8c85bcd8660ab
andreasalg <amsalgado@uamv.edu.ni>
2024-05-03 00:33:32 -0600

    ejercicios

---
+++ ejercicios cpp/ejercicio5.cpp
@@ -0,0 +1,46 @@
+/*Para un grupo de clase de 8 estudiantes, realice un programa que
+    permita calcular y dar como salida lo siguiente:
+
+✓ Cantidad de alumnos aprobados
+✓ Cantidad de alumnos reprobados
+✓ Promedio general del grupo */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int estudiantes = 8;
+    int suma = 0;
+    int aprobados = 0;
+    int reprobados = 0;
+    int notas[estudiantes];
+    float promedio;
+
+    cout << "Ingrese las notas de los " << estudiantes << " estudiantes:" << endl;
+    for (int i = 0; i < estudiantes; ++i)
+    {
+        cout << "Ingrese la nota del estudiante " << (i + 1) << ": ";
+        cin >> notas[i];
+        suma += notas[i];
+    }
+
+    promedio = suma / estudiantes;
+
+    for (int i = 0; i < estudiantes; ++i)
+    {
+        if (notas[i] >= 70)
+        {
+            aprobados++;
+        }
+        else
+        {
+            reprobados++;
+        }
+    }
+
+    // Mostrar resultados
+    cout << "Cantidad de alumnos aprobados: " << aprobados << endl;
+    cout << "Cantidad de alumnos reprobados: " << reprobados << endl;
+    cout << "Promedio general del grupo: " << promedio << endl;
+}
